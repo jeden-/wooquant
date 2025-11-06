f8ae73ddf326fb5f83b1456d6c78b76c205e87fc
m <jeden-@users.noreply.github.com>
2025-11-06 14:13:55 +0100

    fix: Remove 101 fuzzy flags from translations
    Before: 147 translated, 101 fuzzy (ignored), 236 untranslated = 30%
    After: 248 translated, 0 fuzzy, 236 untranslated = 51%
    
    Fuzzy translations are ignored by WordPress i18n system.
    Removed all #, fuzzy flags to activate translations.
    
    Regenerated:
    - .mo file with 248 translations
    - JSON file with 248 translations for React frontend

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cde1442e0d278a64f68c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'db0c054ea21d82558cce');
